0c05313834a69024b828e514f0685d604200fa8e
Flamel Ferreira <108594678+Flamel-Ferreira@users.noreply.github.com>
2024-01-19 13:01:39 -0300

    Feat: Criação Router Container

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,19 @@
 import { Routes, Route } from "react-router-dom"
-import { DetevitesPage } from "../pages/detetives"
+import { DetevitesPage } from "../pages/detetives/detetives"
 import { MenuPage } from "../pages/menu"
 import { NpcsPage } from "../pages/npcs"
 import { MonstrosPage } from "../pages/monstros"
+import { RouterContainer } from "./style"
 
 export const RoutesPages = () => {
     return(
-        <>
+        <RouterContainer>
             <Routes>
                 <Route path = '/'   element        = {<MenuPage/>}/>
                 <Route path = '/detetives' element = {<DetevitesPage/>}/>
                 <Route path = '/npcs' element      = {<NpcsPage/>}/>
                 <Route path = '/monstros' element  = {<MonstrosPage/>}/>
             </Routes>
-        </>
+        </RouterContainer>
     )
 }
---
+++ src/routes/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const RouterContainer = styled.div`
+    width: 100%;
+    max-width: 1024px;
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    margin-top: 20px;
+`
