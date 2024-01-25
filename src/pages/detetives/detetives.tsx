cb83595c65c7edcd15e30be1a3fd017977af1578
Flamel Ferreira <108594678+Flamel-Ferreira@users.noreply.github.com>
2024-01-24 23:24:20 -0300

    Feat: Configurando Requisição e Renderização

--- src/pages/detetives/detetives.tsx
+++ src/pages/detetives/detetives.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState} from "react"
 import { FichaDetetive } from "../../components/FichaDetetive"
 import { DetetivesContainer } from "./style"
+import { api } from "../../services"
+// import { iFichaDetetive } from "./types"
+
 export const DetevitesPage = () => {
+
+    const [list,setList] = useState([])
+
+    const handleListChar = async () =>{
+        const dados = await api.get('/detetives')
+
+        try{
+            setList(dados.data)
+        }catch{
+            console.error()
+        }
+    }
+
+    useEffect(() => {
+        handleListChar()
+    }, [])
+
     return(
         <DetetivesContainer>
-            <FichaDetetive/>
+
+            {list && list.map((detetive)=>{
+                return(
+                    <FichaDetetive propriedadeFicha={detetive}/>
+                )
+            })}
         </DetetivesContainer>
     )
 } 
